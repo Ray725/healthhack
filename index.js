d5abe4f05e424603379a3bde5ccaabc757d4fa09
Ray725 <Ray725@users.noreply.github.com>
2018-04-14 14:41:15 -0400

    ready for presentation

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var ToneAnalyzer3 = require("watson-developer-cloud/tone-analyzer/v3");
+
+var toneanalyzer = new ToneAnalyzer3({
+  url: "https://gateway.watsonplatform.net/tone-analyzer/api",
+  username: "{username}",
+  password: "{password}",
+  version_date: "2017-09-21"
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('index.html');
+  res.sendFile('/Users/raymondli/healthhack/index.html');
 });
 
 app.post('/postdemo', (req, res) => {
-  console.log(req);
+  console.log(req.body);
+  var params = {
+    'tone_input': { 'text': req.body.feeling },
+    'content_type': 'application/json'
+  }
+
+  toneanalyzer.tone(params, (err, response) => {
+    if(err) {
+      throw err;
+    }
+    res.send(JSON.stringify(response));
+  });
+  /*
+  if(req.body.feeling.includes("bad")) {
+    res.send("i'm sorry");
+  } else {
+    res.send("yay");
+  }
+  */
 });
 
 app.listen(3000, () => {
